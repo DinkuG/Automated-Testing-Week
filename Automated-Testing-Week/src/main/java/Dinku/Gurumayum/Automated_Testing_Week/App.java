de162417976add28d4030ed0311553841d7d310b
mnawaz32 <mujahidnawaz@hotmail.com>
2017-10-09 16:52:35 +0000

    init commit

---
+++ Automated-Testing-Week/src/main/java/Dinku/Gurumayum/Automated_Testing_Week/App.java
@@ -0,0 +1,13 @@
+package Dinku.Gurumayum.Automated_Testing_Week;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
